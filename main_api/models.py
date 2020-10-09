019296936a6ccab9eb17d862021724a043c8ffc0
Trevor Youngquist <ytrevor81@gmail.com>
2020-10-09 15:42:59 +0700

    search function upgraded

--- main_api/models.py
+++ main_api/models.py
@@ -6,6 +6,7 @@ class Deck(models.Model):
     deck_name = models.CharField(max_length=25)
     description = models.CharField(max_length=250)
     words = models.TextField()
+    tags = models.CharField(max_length=100, null=True, blank=True)
     published = models.DateField("Uploaded on")
     downloads = models.IntegerField(null=True)
 
@@ -16,6 +17,7 @@ class User(models.Model):
     user = models.CharField(max_length=50)
     user_email = models.CharField(max_length=100, null=True)
     date_joined = models.DateField("Account created on")
+    #password?
 
     def __str__(self):
         return self.user
--- main_api/views.py
+++ main_api/views.py
@@ -7,20 +7,32 @@ from .models import Deck
 
 
 #Function for search function
-def names_list(just_names_list, deck_query):
+def search_list(given_list, search):
     new_list = []
-    deck_name = deck_query.replace("%20", " ")
-    for i in just_names_list:
-        print(i + " " + deck_name)
-        if deck_name in i.lower():
+    for i in given_list:
+        if i == None:
+            pass
+        elif search in i.lower():
             new_list.append(i)
     return new_list
+
+#search the hashtags
+def accurate_names(raw_names, raw_tags, raw_descriptions):
+    ids_from_names = [i.id for i in raw_names] #extracts only the names of each deck
+    ids_from_tags = [i.id for i in raw_tags] #extracts only the hashtags of each deck
+    ids_from_descriptions = [i.id for i in raw_descriptions] #extracts only the descriptions of each deck
+
+    combined_list = ids_from_names + ids_from_tags + ids_from_descriptions
+    returned_list = list(dict.fromkeys(combined_list))
+
+    return returned_list
 
 #All API uses
 @api_view(['GET'])
 def api_overview(request):
     api_urls = {
-        'Explore':'/explore/',  #GET all decks for Explore Decks page
+        'Top Decks':'/topdecks/', #GET for the top ten most downloaded decks
+        'New Decks':'/newdecks/', #GET for the 20 newest decks
         'New Account':'/account/',  #POST create new account
         'Search':'/search/<str:deck_name>/',    #GET one deck for download onto local device
         'Download':'/download/<str:pk>/',    #GET one deck for download onto local device
@@ -38,15 +50,42 @@ def explore_list(request):
     serializer = DeckSerializer(topdecks, many=True)
     return Response(serializer.data)
 
+@api_view(['GET'])
+def topdecks(request):
+    decks = Deck.objects.all().order_by('-downloads') #returns list of all decks in correct order from most downloads to least downloads
+    topdecks = decks[:10]
+    serializer = DeckSerializer(topdecks, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def newdecks(request):
+    decks = Deck.objects.all().order_by('-id')
+    newdecks = decks[:20]
+    serializer = DeckSerializer(newdecks, many=True)
+    return Response(serializer.data)
+
 #Retreives decks for search decks
 @api_view(['GET'])
 def search(request, deck_query):
     all_decks = Deck.objects.all()
-    just_names = [i.deck_name for i in all_decks] #extracts only the names of each deck
+    query = deck_query.replace("%20", "").lower()
 
-    name_query = names_list(just_names, deck_query) #sorts the names matching the search query
+    all_names = [i.deck_name for i in all_decks]
+    all_tags = [i.tags for i in all_decks]
+    all_descriptions = [i.description for i in all_decks]
 
-    decks = Deck.objects.filter(deck_name__in=name_query).order_by('-downloads') #gets the decks matching the elements of name_query
+    names_ofdecks = search_list(all_names, query)
+    tags_ofdecks = search_list(all_tags, query)
+    description_ofdecks = search_list(all_descriptions, query)
+
+    decknames = Deck.objects.filter(deck_name__in=names_ofdecks)
+    decksby_tag= Deck.objects.filter(tags__in=tags_ofdecks)
+    decksby_description = Deck.objects.filter(description__in=description_ofdecks)
+
+    officialmodelquery = accurate_names(decknames, decksby_tag, decksby_description)
+
+    decks = Deck.objects.filter(id__in=officialmodelquery).order_by('-downloads')
+
     topdecks = decks[:20]
     serializer = DeckSerializer(topdecks, many=True)
     return Response(serializer.data)
@@ -67,6 +106,15 @@ def upload(request):
         serializer.save()
 
     return Response(serializer.data)
+
+#@api_view(['POST'])
+#def uploadwithhashtags(request):
+#    serializer = DeckSerializer(data=request.data)
+
+#    if serializer.is_valid():
+#        serializer.save()
+
+#    return Response(serializer.data)
 
 #Inputs new users
 @api_view(['POST'])
